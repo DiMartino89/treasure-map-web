b9efb7683ebb7a3b382a0cce116dfaffd3426467
Martin <spitfire-24@web.de>
2016-09-06 16:53:47 +0200

    Stand_06.09.2016

--- server/config/express.js
+++ server/config/express.js
@@ -47,14 +47,7 @@ module.exports = function(app) {
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
   }
-  /*
-   if ('production' === env) {
-	app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
-    app.use(morgan('dev'));
-  }
-  */
+ 
   if ('development' === env || 'test' === env) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
