0aee2376b9c53dd0c8d71b738a26f97635ce0946
Martin <spitfire-24@web.de>
2016-09-06 23:01:28 +0200

    Stand_06.09.2016

--- server/config/express.js
+++ server/config/express.js
@@ -43,8 +43,11 @@ module.exports = function(app) {
   
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'client/', 'favicon.ico')));
-	app.use(express.static(path.join(config.root)));
-    app.set('appPath', config.root);
+	app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'client')));
+	//app.use(express.static(path.join(config.root)));
+	app.set('appPath', 'client');
+    //app.set('appPath', config.root);
     app.use(morgan('dev'));
   }
  
